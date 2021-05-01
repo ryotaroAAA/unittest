a99f72f7d4daa9dbcb2f305ac85f9995710d9b3d
ryotaro <ryo.ex3991@gmail.com>
2021-05-02 01:58:59 +0900

    /testsから/mainが見えねえ...

---
+++ pytests/src/say.py
@@ -0,0 +1,13 @@
+class Foo:
+    def say(self):
+        return 'foo'
+
+    def say2(self):
+        return 'foo2'
+
+class Hoge:
+    def say(self):
+        return 'hoge'
+
+    def say2(self):
+        return 'hoge2'
